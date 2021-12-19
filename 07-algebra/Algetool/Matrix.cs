80c6a9e056e8d02e0431168aa4d11b958f1025b1
Rocio Cruz Linares <rocio.x.linares95@gmail.com>
2021-12-19 12:10:01 -0500

    Create dotnet solution and projects

---
+++ 07-algebra/Algetool/Matrix.cs
@@ -0,0 +1,5 @@
+namespace MatCom.Algetool;
+
+public class Matrix
+{
+}
---
+++ 07-algebra/App/Program.cs
@@ -0,0 +1,10 @@
+﻿using MatCom.Algetool;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("✅ Everything OK!");
+    }
+}
