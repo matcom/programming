aeaf8e914ac909c97d8bcf18a629ab6abddfee4c
Alejandro Piad <apiad@apiad.net>
2022-01-20 22:23:17 -0500

    Testers

--- exams/2022-01-21/tourist/tester/Program.cs
+++ exams/2022-01-21/tourist/tester/Program.cs
@@ -6,6 +6,7 @@ class Program
     static void Main()
     {
         Case1();
+        Case2();
     }
 
     static void Case1()
@@ -30,12 +31,48 @@ class Program
         PrintArray(respuesta);
     }
 
+    static void Case2()
+    {
+        // Entrada
+        bool[,] mapa =
+        {
+            {false, false, false, false, false, false, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false, false},
+            {true,  true,  true,  true,  false, false, true,  false, false, false},
+            {false, false, false, true,  false, false, true,  false, false, false},
+            {false, false, false, true,  false, false, true,  false, false, false},
+            {false, false, false, true,  false, false, true,  false, true,  false},
+            {false, false, false, true,  true,  true,  true,  false, true,  false},
+            {false, false, false, false, false, false, false, false, true,  false},
+        };
+        int[,] posiciones = {{7,1}, {4,4}, {1,8}};
+
+        // Salida esperada
+        int[,] esperado =
+        {
+            {6,5,4,4,4,3,2,1,1,1},
+            {6,5,4,3,3,3,2,1,0,1},
+            {-10,-10,-10,-10,2,2,-10,1,1,1},
+            {4,4,4,-10,1,1,-10,2,2,2},
+            {3,3,3,-10,0,1,-10,3,3,3},
+            {2,2,2,-10,1,1,-10,4,-10,4},
+            {1,1,1,-10,-10,-10,-10,5,-10,5},
+            {1,0,1,2,3,4,5,6,-10,6},
+        };
+
+        PrintArray(esperado);
+
+        int[,] respuesta = Senderismo.CalculaDistancias(mapa, posiciones);
+        PrintArray(respuesta);
+    }
+
+
     static void PrintArray(int[,] array)
     {
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
-                Console.Write(array[i, j].ToString().PadLeft(3));
+                Console.Write(array[i, j].ToString().PadLeft(5));
 
             Console.WriteLine();
         }
--- exams/2022-01-21/wifi/tester/Program.cs
+++ exams/2022-01-21/wifi/tester/Program.cs
@@ -5,7 +5,8 @@ class Program
 {
     static void Main()
     {
-        Case1();
+        // Case1();
+        Case2();
     }
 
     static void Case1()
@@ -31,12 +32,46 @@ class Program
         PrintArray(respuesta);
     }
 
+    static void Case2()
+    {
+        // Entrada
+        bool[,] mapa =
+        {
+            {false, false, false, false, false, false, false, false},
+            {true,  true,  true,  false, false, false, false, false},
+            {false, false, true,  false, true,  true,  true,  true},
+            {false, false, true,  false, true,  false, false, false},
+            {false, false, true,  false, true,  false, false, false},
+            {false, false, false, false, true,  true,  true,  false},
+            {false, false, false, false, false, false, false, false},
+        };
+        int[,] posiciones = {{2,1}, {4,6}};
+        int max = 10;
+
+        // Salida esperada
+        int[,] esperado =
+        {
+            {0,1,2,2,2,2,1,0},
+            {-10,-10,-10,4,4,2,1,0},
+            {10,10,-10,6,-10,-10,-10,-10},
+            {11,11,-10,8,-10,9,9,9},
+            {10,11,-10,10,-10,9,10,10},
+            {9,10,11,11,-10,-10,-10,11},
+            {8,9,10,11,11,11,11,10},
+        };
+
+        PrintArray(esperado);
+
+        int[,] respuesta = Wifi.IntensidadDeSeñal(mapa, posiciones, max);
+        PrintArray(respuesta);
+    }
+
     static void PrintArray(int[,] array)
     {
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
-                Console.Write(array[i, j].ToString().PadLeft(3));
+                Console.Write(array[i, j].ToString().PadLeft(5));
 
             Console.WriteLine();
         }
