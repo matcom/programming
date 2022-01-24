7fcd3f62474033cb51e9c701af276deb0aa04dc8
Alejandro Piad <apiad@apiad.net>
2022-01-23 20:41:44 -0500

    Update

--- conferences/09-backtrack/backtrack/Square.cs
+++ conferences/09-backtrack/backtrack/Square.cs
@@ -8,27 +8,39 @@ public static class Square
         int[,] square = new int[size, size];
         int max = size * size;
         int sum = max * (max + 1) / (2 * size);
+        bool[] used = new bool[max+1];
 
-        if (Solve(square, 0, 0, sum, max))
+        if (Solve(square, 0, 0, sum, max, used))
             return square;
 
         return null;
     }
 
-    private static bool Solve(int[,] square, int row, int col, int sum, int max)
+    private static bool Solve(int[,] square, int row, int col, int sum, int max, bool[] used)
     {
-        if (row >= square.GetLength(0))
-            return Solve(square, 0, col+1, sum, max);
-
         if (col >= square.GetLength(1))
             return IsValid(square, sum, max);
 
+        if (row >= square.GetLength(0))
+        {
+            if (SumCol(square, col) != sum)
+                return false;
+
+            return Solve(square, 0, col+1, sum, max, used);
+        }
+
         for (int value = 1; value <= max; value++)
         {
+            if (used[value]) continue;
+
             square[row, col] = value;
 
-            if (Solve(square, row+1, col, sum, max))
+            used[value] = true;
+
+            if (Solve(square, row+1, col, sum, max, used))
                 return true;
+
+            used[value] = false;
         }
 
         return false;
@@ -44,15 +56,15 @@ public static class Square
             if (SumCol(square, col) != sum)
                 return false;
 
-        bool[] used = new bool[max];
+        bool[] used = new bool[max+1];
 
         for (int i = 0; i < square.GetLength(0); i++)
         {
             for (int j = 0; j < square.GetLength(1); j++)
             {
-                int val = square[i,j] - 1;
+                int val = square[i,j];
 
-                if (val >= max)
+                if (val > max)
                     return false;
 
                 if (used[val])
@@ -85,3 +97,9 @@ public static class Square
         return sum;
     }
 }
+
+// EJERCICIOS
+
+// 1- Verificar la suma de cada fila completada parcialmente para acelerar la búsqueda.
+// 2- Adicionar una restricción de que las diagonales deben ambas sumar lo mismo.
+// 3- Implementar una variante que en vez de devolver una solución, devuelva la cantidad total de formas de resolverlo.
