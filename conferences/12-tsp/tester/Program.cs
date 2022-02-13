2e301747d9430707b9e81c5442d3d4b0758bd7c2
Alejandro Piad <apiad@apiad.net>
2022-02-12 23:09:22 -0500

    Add tsp

---
+++ conferences/12-tsp/tester/Program.cs
@@ -0,0 +1,51 @@
+﻿using MatCom.Algorithms;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int size = int.Parse(args[0]);
+        var problem = TSPProblem.Random(size);
+
+        Print(problem);
+
+        int time = Environment.TickCount;
+        var solution = TSPSolution.Find(problem);
+        int elapsed = Environment.TickCount - time;
+
+        Console.WriteLine($"\nSolved in {elapsed / 1000} seconds.");
+        Print(solution);
+    }
+
+    static void Print(TSPProblem problem)
+    {
+        Console.Write("   ");
+
+        for (int i = 0; i < problem.Size; i++)
+            Console.Write(problem.Node(i).PadLeft(3));
+
+        Console.WriteLine();
+
+        for (int i = 0; i < problem.Size; i++)
+        {
+            Console.Write(problem.Node(i).PadLeft(3));
+
+            for (int j = 0; j < problem.Size; j++)
+                Console.Write(problem.Cost(i,j).ToString().PadLeft(3));
+
+            Console.WriteLine();
+        }
+    }
+
+    static void Print(TSPSolution solution)
+    {
+        Console.WriteLine($"Total cost: {solution.TotalCost}");
+
+        for (int i = 0; i < solution.Size; i++)
+        {
+            (string start, string end, int cost) = solution.Step(i);
+            Console.WriteLine($"{start} -> {end} = {cost}");
+        }
+    }
+}
---
+++ conferences/12-tsp/tsp/TSP.cs
@@ -0,0 +1,132 @@
+﻿namespace MatCom.Algorithms
+{
+    public class TSPProblem
+    {
+        string[] nodes;
+        int[,] costs;
+
+        public int Size
+        {
+            get { return this.costs.GetLength(0); }
+        }
+
+        public TSPProblem(string[] nodes, int[,] costs)
+        {
+            this.nodes = (string[])nodes.Clone();
+            this.costs = (int[,])costs.Clone();
+        }
+
+        public int Cost(int origin, int destination)
+        {
+            return this.costs[origin, destination];
+        }
+
+        public string Node(int i)
+        {
+            return this.nodes[i];
+        }
+
+        public int Cost(int[] indices, bool closed = false)
+        {
+            int totalCost = 0;
+
+            for (int i = 1; i < indices.Length; i++)
+                totalCost += this.Cost(indices[i - 1], indices[i]);
+
+            if (closed)
+                totalCost += this.Cost(indices[indices.Length - 1], indices[0]);
+
+            return totalCost;
+        }
+
+        public static TSPProblem Random(int size)
+        {
+            string[] nodes = new string[size];
+            Random r = new Random();
+
+            for (int i = 0; i < size; i++)
+                nodes[i] = ((char)('A' + i)).ToString();
+
+            int[,] costs = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == j) continue;
+
+                    costs[i, j] = costs[j, i] = r.Next(1, 10);
+                }
+
+            return new TSPProblem(nodes, costs);
+        }
+    }
+
+    public class TSPSolution
+    {
+        TSPProblem problem;
+        int[] indices;
+
+        public int TotalCost { get; private set; }
+
+        public int Size { get { return this.problem.Size; } }
+
+        public (string, string, int) Step(int i)
+        {
+            int start, end;
+
+            if (i < this.Size - 1)
+            {
+                start = this.indices[i];
+                end = this.indices[i + 1];
+            }
+            else
+            {
+                start = this.indices[i];
+                end = this.indices[0];
+            }
+
+            return (this.problem.Node(start),
+                    this.problem.Node(end),
+                    this.problem.Cost(start, end));
+        }
+
+        public TSPSolution(TSPProblem problem, int[] indices)
+        {
+            this.problem = problem;
+            this.indices = (int[])indices.Clone();
+            this.TotalCost = problem.Cost(indices, closed: true);
+        }
+
+        public static TSPSolution Find(TSPProblem problem)
+        {
+            int[] solution = new int[problem.Size];
+            bool[] visited = new bool[problem.Size];
+            return Solve(problem, solution, visited, 0);
+        }
+
+        private static TSPSolution Solve(TSPProblem problem, int[] solution, bool[] visited, int current)
+        {
+            if (current >= problem.Size)
+                return new TSPSolution(problem, solution);
+
+            TSPSolution? best = null;
+
+            for (int node = 0; node < problem.Size; node++)
+            {
+                if (visited[node]) continue;
+
+                solution[current] = node;
+                visited[node] = true;
+
+                TSPSolution s = Solve(problem, solution, visited, current + 1);
+
+                if (best == null || s.TotalCost < best.TotalCost)
+                    best = s;
+
+                visited[node] = false;
+            }
+
+            return best!;
+        }
+    }
+}
