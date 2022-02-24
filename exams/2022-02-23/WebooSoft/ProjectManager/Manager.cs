1940cc611cdf1775b2815d061e2fe2eb0117d6f0
Alejandro Piad <apiad@apiad.net>
2022-02-24 15:10:27 -0500

    Prueba rec

---
+++ exams/2022-02-23/WebooSoft/ProjectManager/Manager.cs
@@ -0,0 +1,11 @@
+﻿namespace Weboo.Examen
+{
+    public class Manager
+    {
+        public static double DuracionProyecto(int[] tareas, double[,] desarrolladores)
+        {
+            // Borre esta línea y agregue su código
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ exams/2022-02-23/WebooSoft/Tester/Program.cs
@@ -0,0 +1,36 @@
+﻿﻿using Weboo.Examen;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        // Adicione aquí los tests que considere necesarios
+        Test(
+            tareas: new[] { 5, 10, 16 },
+            desarrolladores: new double[,]
+            {
+                { 1.0, 0.5, 2.0 },
+                { 2.0, 1.0, 0.5 },
+            },
+            esperado: 9
+        );
+    }
+
+    public static void Test(int[] tareas, double[,] desarrolladores, double esperado)
+    {
+        try {
+
+            double resultado = Manager.DuracionProyecto(tareas, desarrolladores);
+            if (resultado != esperado) {
+                throw new Exception($"Se esperaba {esperado} pero se obtuvo {resultado}");
+            }
+
+            Console.WriteLine($"🟢 Resultado correcto: {resultado}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"🔴 {e}");
+        }
+    }
+}
