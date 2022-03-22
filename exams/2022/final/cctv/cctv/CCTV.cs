fc75a282fd077d82590ca2ce2cfea73b487ea95c
Alejandro Piad <apiad@apiad.net>
2022-03-22 19:55:42 -0400

    Update

---
+++ exams/2022/final/cctv/cctv/CCTV.cs
@@ -0,0 +1,12 @@
+﻿namespace Weboo.Examen
+{
+    public class CCTV
+    {
+        public static int UbicarCamaras(bool[,] intersecciones)
+        {
+            // Borre esta línea y escriba su código
+            throw new NotImplementedException();
+        }
+    }
+
+}
