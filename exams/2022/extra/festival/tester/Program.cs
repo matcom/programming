9d530311520ad0bf73a79005e0013a10746db74c
Alejandro Piad <apiad@apiad.net>
2022-03-31 17:10:11 -0400

    Festival

---
+++ exams/2022/extra/festival/tester/Program.cs
@@ -0,0 +1,77 @@
+﻿using Weboo.Examen;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        // Adicione aquí los tests que considere necesarios
+
+        // Todos son amigos de todos, así que serían 3 equipos de una persona, pero no
+        // pueden haber equipos de una persona
+        Test(
+            // Amigos
+            new[,]
+            {
+                {false,  true,  true},
+                { true, false,  true},
+                { true,  true, false},
+            },
+            // Resultado esperado
+            0
+        );
+
+        // Dos conjuntos disjuntos de amigos, pero se queda un equipo
+        // con una sola persona que no se cuenta
+        Test(
+            // Amigos
+            new[,]
+            {
+                // Estos tres están conectados entre sí
+                {false,  true,  true, false, false},
+                { true, false,  true, false, false},
+                { true,  true, false, false, false},
+                // Estos dos están conectados entre sí
+                {false,  false, false, false, true},
+                {false,  false, false, true, false},
+            },
+            // Resultado esperado
+            2
+        );
+
+        // Nadie conoce a nadie
+        Test(
+            // Amigos
+            new[,]
+            {
+                {false, false, false, false, false, false},
+                {false, false, false, false, false, false},
+                {false, false, false, false, false, false},
+                {false, false, false, false, false, false},
+                {false, false, false, false, false, false},
+                {false, false, false, false, false, false},
+            },
+            // Resultado esperado
+            1
+        );
+    }
+
+    public static void Test(bool[,] amigos, int esperado)
+    {
+        try
+        {
+            int resultado = Festival.MenorCantidadEquipos(amigos);
+
+            if (resultado != esperado)
+            {
+                throw new Exception($"Se esperaba {esperado} pero se obtuvo {resultado}");
+            }
+
+            Console.WriteLine($"🟢 Resultado correcto: {resultado}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"🔴 {e}");
+        }
+    }
+}
