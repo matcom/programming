541e9a1dbfa0dac48211b23eb7ff00c992ee1670
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-05-07 00:39:03 -0400

    Update Confernece 14
    1. Implementation of the Confernce 14
    2. Print Expression with minimum amount of parentheses

--- conferences/14-evaluator/evaluator/Expression.cs
+++ conferences/14-evaluator/evaluator/Expression.cs
@@ -1,212 +1,264 @@
-﻿namespace Weboo.Evaluator
-{
+﻿namespace Weboo.Evaluator;
+public abstract class Expresion {
+    public abstract double Result();
+    public abstract string Type();
+    public abstract string TypeParent();
+}
 
-    public abstract class Expression
-    {
-        public abstract double Evaluate();
+public class Constant : Expresion {
+    double value;
+    public Constant(double value) {
+        this.value = value;
     }
-
-    public abstract class BinaryExpression : Expression
-    {
-        protected readonly Expression left;
-        protected readonly Expression right;
-
-        public BinaryExpression(Expression left, Expression right)
-        {
-            this.left = left;
-            this.right = right;
-        }
-
-        public override double Evaluate()
-        {
-            double leftValue = this.left.Evaluate();
-            double rightValue = this.right.Evaluate();
-
-            return this.Evaluate(leftValue, rightValue);
-        }
-
-        protected abstract double Evaluate(double left, double right);
+    public override string Type() {
+        return "Constant";
     }
-
-    public class Add : BinaryExpression
-    {
-        public Add(Expression left, Expression right) : base(left, right)
-        {
-
-        }
-
-        protected override double Evaluate(double left, double right)
-        {
-            return left + right;
-        }
-
-        public override string ToString()
-        {
-            return $"({left.ToString()}) + ({right.ToString()})";
-        }
+    public override string TypeParent() {
+        return "Constant";
     }
-
-    public class Subtract : BinaryExpression
-    {
-        public Subtract(Expression left, Expression right) : base(left, right)
-        {
-
-        }
-
-        protected override double Evaluate(double left, double right)
-        {
-            return left - right;
-        }
-
-        public override string ToString()
-        {
-            return $"({left.ToString()}) - ({right.ToString()})";
-        }
+    public override double Result() {
+        return this.value;
     }
-
-    public class Multiply : BinaryExpression
-    {
-        public Multiply(Expression left, Expression right) : base(left, right)
-        {
-
-        }
-
-        protected override double Evaluate(double left, double right)
-        {
-            return left * right;
-        }
-
-        public override string ToString()
-        {
-            return $"({left.ToString()}) * ({right.ToString()})";
-        }
-    }
-
-    public class Divide : BinaryExpression
-    {
-        public Divide(Expression left, Expression right) : base(left, right)
-        {
-
-        }
-
-        protected override double Evaluate(double left, double right)
-        {
-            return left / right;
-        }
-
-        public override string ToString()
-        {
-
-            return $"({left.ToString()}) / ({right.ToString()})";
-        }
-    }
-
-    public class Pow : BinaryExpression
-    {
-        public Pow(Expression left, Expression right) : base(left, right)
-        {
-
-        }
-
-        protected override double Evaluate(double left, double right)
-        {
-            return Math.Pow(left, right);
-        }
-
-        public override string ToString()
-        {
-            return $"({left.ToString()}) ^ ({right.ToString()})";
-        }
-    }
-
-    public abstract class UnaryExpression : Expression
-    {
-        protected readonly Expression inner;
-
-        public UnaryExpression(Expression inner)
-        {
-            this.inner = inner;
-        }
-
-        public override double Evaluate()
-        {
-            return this.Evaluate(this.inner.Evaluate());
-        }
-
-        protected abstract double Evaluate(double inner);
-    }
-
-    public class Exp : UnaryExpression
-    {
-        public Exp(Expression inner) : base(inner)
-        {
-
-        }
-
-        protected override double Evaluate(double inner)
-        {
-            return Math.Exp(inner);
-        }
-
-        public override string ToString()
-        {
-            return $"e^({inner.ToString()})";
-        }
-    }
-
-    public class Sin : UnaryExpression
-    {
-        public Sin(Expression inner) : base(inner)
-        {
-
-        }
-
-        protected override double Evaluate(double inner)
-        {
-            return Math.Sin(inner);
-        }
-
-        public override string ToString()
-        {
-            return $"sin({inner.ToString()})";
-        }
-    }
-
-    public class Cos : UnaryExpression
-    {
-        public Cos(Expression inner) : base(inner)
-        {
-
-        }
-
-        protected override double Evaluate(double inner)
-        {
-            return Math.Cos(inner);
-        }
-
-        public override string ToString()
-        {
-            return $"cos({inner.ToString()})";
-        }
-    }
-
-    public class Constant : Expression
-    {
-        double value;
-
-        public Constant(double value)
-        {
-            this.value = value;
-        }
-
-        public override string ToString()
-        {
-            return value.ToString();
-        }
-
-        public override double Evaluate()
-        {
-            return this.value;
-        }
+    public override string ToString() {
+        return $"{this.value}";
     }
 }
+
+
+#region UnaryExpresions
+public abstract class UnaryExpresion : Expresion {
+    protected Expresion value;
+    public UnaryExpresion(Expresion value) {
+        this.value = value;
+    }
+    public override string TypeParent() {
+        return "UnaryExpresion";
+    }
+}
+
+public class Sin : UnaryExpresion {
+    public Sin(Expresion value) : base(value){}
+    public override double Result() {
+        return Math.Sin( this.value.Result() );
+    }
+    public override string ToString() {
+        return $"Sin({ this.value.ToString() })";
+    }
+    public override string Type() {
+        return "Sin";
+    }
+}
+public class Cos : UnaryExpresion {
+    public Cos(Expresion value) : base(value){}
+    public override double Result() {
+        return Math.Cos( this.value.Result() );
+    }
+    public override string ToString() {
+        return $"Cos({ this.value.ToString() })";
+    }
+    public override string Type() {
+        return "Cos";
+    }
+}
+public class Absolute : UnaryExpresion {
+    public Absolute(Expresion value) : base(value){}
+    public override double Result() {
+        return Math.Abs( this.value.Result() );
+    }
+    public override string ToString() {
+        return $"|{ this.value.ToString() }|";
+    }
+    public override string Type() {
+        return "Absolute";
+    }
+}
+
+#endregion
+
+
+#region BinaryExpresions
+public abstract class BinaryExpresion : Expresion {
+    protected Expresion left;
+    protected Expresion right;
+
+    public BinaryExpresion( Expresion left, Expresion right ) {
+        this.left = left;
+        this.right = right;
+    }
+    public override string TypeParent()
+    {
+        return "BinaryExpresion";
+    }
+}
+public class Multiplication : BinaryExpresion {
+    public Multiplication( Expresion left, Expresion right ) : base(left, right) {}
+    public override double Result() {
+        return this.left.Result() * this.right.Result();
+    }
+    public override string ToString() {
+        string LType = this.left.Type();
+        string LTypeParent = this.left.TypeParent();
+        string RType = this.right.Type();
+        string RTypeParent = this.right.TypeParent();
+
+        string l = "", r = "";
+        if( LType == "Constant" ) {
+            l = this.left.ToString();
+        } else
+        if( LTypeParent == "UnaryExpresion" ) l = this.left.ToString();
+        else
+        if( LTypeParent == "BinaryExpresion" ) {
+            if( LType == "Multiplication" || LType == "Division" || LType == "Power" ) l = this.left.ToString();
+            else l = $"({this.left.ToString()})";
+        }
+
+        if( RType == "Constant" ) {
+            if( this.right.Result() < 0 ) r = $"({ this.right.ToString() })";
+            else r = this.right.ToString();
+        } else
+        if( RTypeParent == "UnaryExpresion" ) r = this.right.ToString();
+        else
+        if( RTypeParent == "BinaryExpresion" ) {
+            if( RType == "Multiplication" || RType == "Division" || RType == "Power" ) r = this.right.ToString();
+            else r = $"({this.right.ToString()})";
+        }
+        return l + "*" + r;
+    }
+    public override string Type() {
+        return "Multiplication";
+    }
+}
+public class Division : BinaryExpresion {
+    public Division( Expresion left, Expresion right ) : base(left, right) {}
+    public override double Result() {
+        if( this.right.Result() == 0 ) 
+            throw new  DivideByZeroException();
+        return this.left.Result() / this.right.Result();
+    }
+    public override string ToString() {
+        string LType = this.left.Type();
+        string LTypeParent = this.left.TypeParent();
+        string RType = this.right.Type();
+        string RTypeParent = this.right.TypeParent();
+
+        string l = "", r = "";
+        if( LType == "Constant" ) {
+            l = this.left.ToString();
+        } else
+        if( LTypeParent == "UnaryExpresion" ) l = this.left.ToString();
+        else
+        if( LTypeParent == "BinaryExpresion" ) {
+            if( LType == "Multiplication" || LType == "Division" || LType == "Power" ) l = this.left.ToString();
+            else l = $"({this.left.ToString()})";
+        }
+
+        if( RType == "Constant" ) {
+            if( this.right.Result() < 0 ) r = $"({ this.right.ToString() })";
+            else r = this.right.ToString();
+        } else
+        if( RTypeParent == "UnaryExpresion" ) r = this.right.ToString();
+        else
+        if( RTypeParent == "BinaryExpresion" ) {
+            if( RType == "Multiplication" || RType == "Division" || RType == "Power" ) r = this.right.ToString();
+            else r = $"({this.right.ToString()})";
+        }
+        return l + "/" + r;
+    }
+    public override string Type() {
+        return "Division";
+    }
+}
+public class Addition : BinaryExpresion {
+    public Addition( Expresion left, Expresion right ) : base(left, right) {}
+    public override double Result() {
+        return this.left.Result() + this.right.Result();
+    }
+    public override string ToString() {
+        string LType = this.left.Type();
+        string LTypeParent = this.left.TypeParent();
+        string RType = this.right.Type();
+        string RTypeParent = this.right.TypeParent();
+
+        string l = "", r = "";
+        if( LType == "Constant" ) {
+             l = this.left.ToString();
+        } else
+        if( LTypeParent == "UnaryExpresion" ) l = this.left.ToString();
+        else
+        if( LTypeParent == "BinaryExpresion" ) {
+            if( LType == "Multiplication" || LType == "Division" || LType == "Power" ) l = this.left.ToString();
+            else l = $"({this.left.ToString()})";
+        }
+
+        if( RType == "Constant" ) {
+             r = this.right.ToString();
+        } else
+        if( RTypeParent == "UnaryExpresion" ) r = this.right.ToString();
+        else
+        if( RTypeParent == "BinaryExpresion" ) {
+            if( RType == "Multiplication" || RType == "Division" || RType == "Power" ) r = this.right.ToString();
+            else r = $"({this.right.ToString()})";
+        }
+
+        if( RType == "Constant" && this.right.Result() < 0 )
+            return l + r;
+        return l + "+" + r;
+    }
+    public override string Type() {
+        return "Addition";
+    }
+}
+public class Subtraction : BinaryExpresion {
+    public Subtraction( Expresion left, Expresion right ) : base(left, right) {}
+    public override double Result() {
+        return this.left.Result() - this.right.Result();
+    }
+    public override string ToString() {
+        string LType = this.left.Type();
+        string LTypeParent = this.left.TypeParent();
+        string RType = this.right.Type();
+        string RTypeParent = this.right.TypeParent();
+
+        string l = "", r = "";
+        if( LType == "Constant" ) {
+             l = this.left.ToString();
+        } else
+        if( LTypeParent == "UnaryExpresion" ) l = this.left.ToString();
+        else
+        if( LTypeParent == "BinaryExpresion" ) {
+            if( LType == "Multiplication" || LType == "Division" || LType == "Power" ) l = this.left.ToString();
+            else l = $"({this.left.ToString()})";
+        }
+
+        if( RType == "Constant" ) {
+            r = this.right.ToString();
+        } else
+        if( RTypeParent == "UnaryExpresion" ) r = this.right.ToString();
+        else
+        if( RTypeParent == "BinaryExpresion" ) {
+            if( RType == "Multiplication" || RType == "Division" || RType == "Power" ) r = this.right.ToString();
+            else r = $"({this.right.ToString()})";
+        }
+
+        if( RType == "Constant" && this.right.Result() < 0 )
+            return l + "+" + Math.Abs(this.right.Result());
+        return l + "-" + r;
+    }
+    public override string Type() {
+        return "Subtraction";
+    }
+}
+public class Power : BinaryExpresion {
+    public Power( Expresion left, Expresion right ) : base(left, right) {}
+    public override double Result() {
+        return Math.Pow(this.left.Result(), this.right.Result());
+    }
+    public override string ToString() {
+        return $"Pow({ this.left.ToString() }, { this.right.ToString() })";
+    }
+    public override string Type() {
+        return "Power";
+    }
+}
+
+#endregion
--- conferences/14-evaluator/tester/Program.cs
+++ conferences/14-evaluator/tester/Program.cs
@@ -5,23 +5,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Expression e = GetExpression();
-        Console.WriteLine($"{e} = {e.Evaluate()}");
+        Test1();
+        Test2();
+        Test3();
+        Test4();
     }
 
-    static Expression GetExpression() {
-        return new Divide(
+    static void Test1() {
+
+        Expresion e = new Division(
             new Sin(new Constant(4)),
-            new Multiply(
-                new Subtract(
+            new Multiplication(
+                new Subtraction(
                     new Cos(new Constant(7)),
-                    new Exp(new Constant(2))
+                    new Constant(2)
                 ),
-                new Pow(
+                new Power(
                     new Constant(3),
                     new Constant(5)
                 )
             )
         );
+
+        Console.WriteLine($"{e.ToString()} = {e.Result()}");
     }
+    static void Test2() {
+        // | Sin( (2*5/3) + (3 - 4) ) * 1000 |
+        Expresion exp1 = new Division( new Multiplication(new Constant(2), new Constant(5)), new Constant(3) );
+        Expresion exp2 = new Subtraction(new Constant(3), new Constant(4));
+        Expresion exp12 = new Addition(exp1, exp2);
+
+        Expresion exp3 = new Multiplication( new Sin(exp12), new Constant(1000) );
+        Expresion e = new Absolute( new Multiplication(exp3, new Constant(-1)) );
+    
+        Console.WriteLine($"{e.ToString()} = {e.Result()}");
+    }
+
+    static void Test3() {
+        // (5 - (4 + 3)) / 2 + (-3)
+        Expresion exp1 = new Division( new Subtraction( new Constant(5), new Addition(new Constant(4), new Constant(3)) ), new Constant(2) );
+        Expresion e = new Addition( exp1, new Constant(-3) );
+
+        Console.WriteLine($"{e.ToString()} = {e.Result()}");
+    }
+    static void Test4() {
+        // -3 - (-3)
+        Expresion e = new Subtraction( new Constant(-3), new Constant(-3) );
+        Console.WriteLine($"{e.ToString()} = {e.Result()}");
+    }
+
 }
