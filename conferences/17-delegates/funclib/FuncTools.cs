f02c49252eb59394415ec60eee80562ab4ab218b
Alejandro Piad <apiad@apiad.net>
2022-05-30 22:54:06 -0400

    Delegates

---
+++ conferences/17-delegates/funclib/FuncTools.cs
@@ -0,0 +1,52 @@
+﻿namespace funclib
+{
+    public delegate TOut Mapping<TIn, TOut>(TIn item);
+
+    public delegate bool Predicate<T>(T item);
+
+    public delegate TResult Reductor<T, TResult>(T current, TResult accum);
+
+    public static class FuncTools
+    {
+        public static T[] Filter<T>(T[] items, Predicate<T> predicate)
+        {
+            T[] result = new T[items.Length];
+            int p = 0;
+
+            foreach (T item in items)
+            {
+                if (predicate(item))
+                {
+                    result[p++] = item;
+                }
+            }
+
+            Array.Resize(ref result, p);
+            return result;
+        }
+
+        public static TOut[] Map<TIn, TOut>(TIn[] items, Mapping<TIn, TOut> mapping)
+        {
+            TOut[] result = new TOut[items.Length];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                result[i] = mapping(items[i]);
+            }
+
+            return result;
+        }
+
+        public static TResult Reduce<T, TResult>(T[] items, Reductor<T, TResult> reductor, TResult seed = default(TResult))
+        {
+            TResult result = seed;
+
+            foreach (var item in items)
+            {
+                result = reductor(item, result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ conferences/17-delegates/tester/Program.cs
@@ -0,0 +1,68 @@
+﻿using funclib;
+
+class Program
+{
+    void Main()
+    {
+        TestFilter();
+    }
+
+    private static void TestFilter()
+    {
+        int[] numbers = new int[100];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i + 1;
+        }
+
+        int[] evenNumbers = FuncTools.Filter(numbers, new Predicate<int>(IsEven));
+
+        int[] oddNumbers = FuncTools.Filter(numbers, new Predicate<int>(delegate (int x)
+        {
+            return x % 2 == 1;
+        }));
+
+        int[] fullNumbers = FuncTools.Filter(numbers, x => x % 10 == 0);
+
+        DateTime[] dates = new DateTime[365];
+
+        for (int i = 0; i < dates.Length; i++)
+        {
+            dates[i] = new DateTime(2022, 1, 1).AddDays(i);
+        }
+
+        DateTime[] weekends = FuncTools.Filter(dates,
+            date => date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday
+        );
+    }
+
+    private static void TestMap()
+    {
+        int[] numbers = new int[100];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i + 1;
+        }
+
+        int[] squares = FuncTools.Map(items, x => x * x);
+    }
+
+    private static void TestReduce()
+    {
+        int[] numbers = new int[100];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i + 1;
+        }
+
+        int sum = FuncTools.Reduce(numbers, (num, accum) => num + accum, seed=0);
+    }
+
+    static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+}
